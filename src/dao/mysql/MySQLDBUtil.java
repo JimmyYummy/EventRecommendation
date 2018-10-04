e8c267ad09bd35ab9d87b2327dfbbc6e14a11cbc
Jimmy <38588445+JimmyYummy@users.noreply.github.com>
2018-10-03 23:43:12 -0400

    Update MySQLDBUtil.java

--- src/dao/mysql/MySQLDBUtil.java
+++ src/dao/mysql/MySQLDBUtil.java
@@ -4,8 +4,8 @@ public class MySQLDBUtil {
 	private final static String HOST_IP = "localhost";
 	private final static String PORT_NUMBER = "3306";
 	private final static String DATABASE_NAME = "tm_event_recommendation";
-	private final static String USER_NAME = "****";
-	private final static String PASSWORD = "****";
+	private final static String USER_NAME = "********";
+	private final static String PASSWORD = "********";
 	final static String URL = String.format(
 			"jdbc:mysql://%s:%s/%s?user=%s&password=%s&autoReconnect=true&serverTimezone=UTC",
 			HOST_IP, PORT_NUMBER, DATABASE_NAME, USER_NAME, PASSWORD);
